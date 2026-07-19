c4f07ac072df3ee65388577e93bcbaec5a05ebf1
soumyalaad <soumyalaad06@gmail.com>
2026-07-19 18:35:15 +0530

    Connect class 10 backend routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,39 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from "dotenv";
+import class10Routes from "./routes/class10Routes.js";
 
-import authRoutes from "./routes/auth.routes.js";
-import streamRoutes from "./routes/streamRoutes.js";
-
-
+dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://groer-x-one.vercel.app",
+      "https://groer-x-blx5dosxl-ajsedtech-bytes-projects.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("GroerX API is running");
+  res.status(200).json({
+    success: true,
+    message: "GroerX API running",
+  });
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/streams", streamRoutes);
+app.use("/api/class10", class10Routes);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`GroerX API running on ${port}`);
+app.listen(PORT, () => {
+  console.log(`GroerX API running on ${PORT}`);
+  console.log("Your service is live 🎉");
 });
